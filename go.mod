4df552e67ac39cd43204e869bf7ce2a0318871fe
Derric Williams <derricw@gmail.com>
2023-01-13 21:39:37 -0800

    initial commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/derricw/soulmsg-server
+
+go 1.18
+
+require (
+	github.com/derricw/soulmsg v0.0.0-20230113231759-7903d2c82bb3 // indirect
+	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/spf13/cobra v1.6.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+)
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/derricw/soulmsg/msg"
+)
+
+func response(w http.ResponseWriter, req *http.Request) {
+	msg := fmt.Sprintf("%s\n", msg.RandomMessage(false))
+	fmt.Fprintf(w, msg)
+}
+
+func main() {
+	httpPort := os.Getenv("HTTP_PORT")
+	if httpPort == "" {
+		httpPort = "8080"
+	}
+	http.HandleFunc("/", response)
+	http.ListenAndServe(":"+httpPort, nil)
+}
